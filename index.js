fd58d88a242554a6ee7d7411c5d5ab3b4a12d532
mamatovmurod8898@gmail.com <islombekqurbonovfd@gmail.com>
2023-04-15 12:14:57 +0500

    project

---
+++ index.js
@@ -0,0 +1,47 @@
+// let sum = 4
+// let pul = 5
+
+// for( ; sum > 5; sum--){
+//    console.log(`i = ${sum}`)
+// }
+
+// while( sum > pul  ){
+//    console.log("i = ", sum)
+//    sum--
+// } 
+
+// do{
+//    console.log("salom")
+//    sum--
+// }while(sum > pul )
+
+// for ( ; sum < 10 ; ){
+   //    console.log(sum);
+   //    sum +=2
+   // }
+   
+   // while(sum > 10){
+      //    console.log(sum)
+      
+//       //    sum -=3
+//       // }
+      
+// let sum = 20
+
+// do{
+//  console.log(sum);
+//  sum++
+// }while(sum < 1)
+
+let i = 0
+let i1 = 0
+let i2 = 0
+while(++i < 5) console.log('i  =  ',i);
+
+while(i1++ < 5) console.log('i1  =  ',i1);
+
+
+while(i2 < 3){
+   console.log(`number ${i2}`);
+   i2++
+}
